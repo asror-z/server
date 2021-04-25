6f35887ae4bf461859cb4c36cbdbec53d47f380e
Gitea <gitea@fake.local>
2021-04-25 17:54:05 +0500

    1333

--- mysql/cmd-stop.cmd
+++ mysql/cmd-stop.cmd
@@ -1,3 +1,3 @@
 call nssm-ALL.cmd
 
-%ProgramPath%\bin\mysqladmin --defaults-file=%ProgramPath%\conf\config.ini -pserverpass1234 shutdown
+%ProgramPath%\bin\mysqladmin --defaults-file=%ProgramPath%\conf\config.ini -proot shutdown
